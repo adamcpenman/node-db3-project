9cfbb5ae5f0f8a7bc5e17f0a51dfdf6e0359af6f
Adam <adamcpenman@gmail.com>
2020-01-07 18:31:07 -0600

    findById()

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -4,11 +4,17 @@ function find() {
     return db("schemes")
 }
 
+function findById(id) {
+    return db("schemes")
+        .where({ id })
+        .first()
+}
+
 
 
 module.exports = {
     find,
-    // findById,
+    findById,
     // findSteps,
     // add,
     // update,
